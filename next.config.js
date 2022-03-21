5b4776175f74984b0d40efa9277d41d934d3479f
ovidubya <ovidubs@gmail.com>
2022-03-20 22:12:50 -0500

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
-module.exports = nextConfig
+// module.exports = nextConfig;
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
+});
